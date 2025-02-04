553e9e39e91805ee03f0f5889c8f91c7f8918b44
Davis Christy Robert <daviscr2006@gmail.com>
2025-02-04 16:19:48 +0530

    Add files via upload

---
+++ new2.c
@@ -0,0 +1,16 @@
+//write a program to read roll no and age of student
+//author davis christy robert
+//date:28-1-2025
+#include <stdio.h>
+int main()
+{
+int roll_no,age;
+printf("enter the roll no:");
+scanf("%d",&roll_no);
+printf("enter the age");
+scanf("%d",&age);
+printf("The roll no is %d\n",roll_no);
+printf("\nthe age is %dyears\n",age);
+return 0;
+}
+
