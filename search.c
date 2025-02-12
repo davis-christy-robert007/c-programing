18109a0abf1a2a0beb1baea4c1cf1284f2d4fac4
Davis Christy Robert <daviscr2006@gmail.com>
2025-02-12 22:58:15 +0530

    Update search.c

--- search.c
+++ search.c
@@ -9,15 +9,25 @@ int main()
         printf("enter the values");
         scanf("%d",&a[i]);
         }
-        int no;
+        int no,pos=0,flag=0;
         printf("enter the value to be searched");
         scanf("%d",&no);
-        for(int i=0,i<n,i++){
+        for(int i=0;i<n;i++){
         if(no==a[i]){
-        
-        printf("the no is found");
+            flag=1;
+            pos=i;
+        }else{
+            flag=0;
         }
 
-return 0;
-
-}
+        }
+        if(flag==0){
+            printf("the number not found");
+        }else{
+            printf("the number is found and its postion is %d",pos);
+        }
+    
+        
+        
+        return 0;
+        }
