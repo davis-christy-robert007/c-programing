c06ff32669aa4c168fe3cd195c9dce99903505c4
Davis Christy Robert <daviscr2006@gmail.com>
2025-03-18 21:47:54 +0530

    Update readarr.c

--- readarr.c
+++ readarr.c
@@ -9,9 +9,17 @@ printf("enter the elemtns");
 for(i=0;i<n;i++){
 scanf("%d",&read[i]);
 }
+
+
 result=sum(read,n);
 printf("The sum is:%d",result);
 return 0;
+}
 int sum(int arr[],int n){
 int sum ;
-for(i=0;i<
+for(int i=0;i<n;i++){
+    sum+=arr[i];
+}
+return sum;
+}
+
