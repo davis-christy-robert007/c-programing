4705a33dc7d0fec76767889e7df9c55ec6150257
Davis Christy Robert <daviscr2006@gmail.com>
2025-04-06 22:39:39 +0530

    Update stud.c

--- stud.c
+++ stud.c
@@ -1,39 +1,55 @@
 #include <stdio.h>
-struct student
-{
-char name[50];
-int rollno;
-int marks[3];
-int total;
-float avg;
+
+struct student {
+    char name[50];
+    int rollno;
+    int marks[3];
+    int total;
+    float avg;
 };
-int main(){
-int i,n,j;
-struct student s[n];
-printf("enter the number of students:");
-scanf("%d",&n);
-for(i=0;i<n,i++){
-printf("enter the details of student :");
-scanf("%d",&n);
-printf("enter the student name");
-scanf("%s",s[i].name);
-printf("enter the rollno");
-scanf("%d",&s[i].rollno);
-s[i].total=0;
-for(j=0;j<3;j++){
-printf("enter the marks for the subject %d",j+1);
-scanf("%d",&s[i].marks[j]);
-s[i].total=s[i].marks[j];
-}
-s[i].avg=s[i].total;
-}
-printf("\n student details \n");
-for(i=0;i<n;i++){
-printf("student:%d\n",i+1);
-printf("name:%s\n",s[i].name);
-printf("roll no:%d\n",s[i].rolllno);
-printf("marks:%d\n",s[i].total);
-printf("Average :%f\n",s[i].avg);
-}
-return 0;
+
+int main() {
+    int i, j, n;
+
+   
+    printf("Enter the number of students: ");
+    scanf("%d", &n);
+
+    struct student s[n];  
+    for(i = 0; i < n; i++) {
+        printf("\nEnter the details of student %d:\n", i + 1);
+
+        printf("Enter the student's name: ");
+        scanf("%s", s[i].name);  
+
+        printf("Enter the roll number: ");
+        scanf("%d", &s[i].rollno);  
+
+        s[i].total = 0;  
+
+        
+        for(j = 0; j < 3; j++) {
+            printf("Enter the marks for subject %d: ", j + 1);
+            scanf("%d", &s[i].marks[j]);
+            s[i].total += s[i].marks[j];  
+        }
+
+        
+        s[i].avg = s[i].total / 3.0;  
+    }
+
+   
+    for(i = 0; i < n; i++) {
+        printf("\nDetails of student %d:\n", i + 1);
+        printf("student name: %s\n", s[i].name);
+        printf("roll_number: %d\n", s[i].rollno);
+        printf("Marks: ");
+        for(j = 0; j < 3; j++) {
+            printf("%d ", s[i].marks[j]);  
+        }
+        printf("\ntotal mark: %d\n", s[i].total);
+        printf("Average mark: %.2f\n", s[i].avg);
+    }
+
+    return 0;  
 }
