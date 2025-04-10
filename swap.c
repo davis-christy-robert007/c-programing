5ccde74ca31264dd419b6b5a0226ee2a3b32b7ad
Davis Christy Robert <daviscr2006@gmail.com>
2025-04-11 00:05:05 +0530

    Add files via upload

--- swap.c
+++ swap.c
@@ -1,16 +1,20 @@
-//write a program to swap two values using a temp variable
-//author davis christy robert
-//date:28-1-2025
-#include <stdio.h>
+#include<stdio.h>
+void swap(int *a, int *b)
+{
+ int temp;
+ temp=*a;
+ *a=*b;
+ *b=temp;
+}
 int main()
 {
-int a,b,temp;
-a=5;
-b=3;
-printf("a=%d and b=%d",a,b);
-temp=a;
-a=b;
-b=temp;
-printf("After swapping, a:%d and b:%d",a,b);
-return 0;
+ int a,b;
+ printf("Enter the number1:");
+ scanf("%d",&a);
+ printf("Enter the number2:");
+ scanf("%d",&b);
+ printf("The numbers before swapping is %d,%d:",a,b);
+ swap(&a,&b);
+ printf(" \n The numbers after swapping is %d,%d:",a,b);
+ return 0;
 }
